bcfb9c63b5147706b3e98e56290a3cb884ee91e8
soniagkalliath <soniageorge111@gmail.com>
2023-05-03 20:07:25 +0530

    register edit page only design complete

--- src/Pages/Edit/Edit.js
+++ src/Pages/Edit/Edit.js
@@ -1,9 +1,91 @@
 import React from 'react';
 import './Edit.css';
+import { Card,Row,Form,Button } from 'react-bootstrap';
+import Select from 'react-select';
 
 function Edit() {
+  // status optios
+  const options = [
+    { value: 'Active', label: 'Active' },
+    { value: 'InActive', label: 'InActive' },
+  ];
   return (
-    <div>Edit</div>
+    <>
+    <div className="container">
+          <h2 className='text-center mt-1'>Update Your Details</h2>
+
+          <Card className='shadow mt-3 p-3'>
+            <div className="profile_div text-center">
+              <img src='https://cdn1.iconfinder.com/data/icons/avatars-1-5/136/88-512.png' alt="img" />
+            </div>
+
+            <Form>
+              <Row>
+                <Form.Group className="mb-3 col-lg-6" controlId="formBasicEmail">
+                  <Form.Label>First name</Form.Label>
+                  {/* value={inputdata.fname} onChange={setInputValue} */}
+                  <Form.Control type="text" name='fname'  placeholder='Enter FirstName' />
+                </Form.Group>
+                <Form.Group className="mb-3 col-lg-6" controlId="formBasicEmail">
+                  <Form.Label>Last Name</Form.Label>
+                  {/* value={inputdata.lname} onChange={setInputValue} */}
+                  <Form.Control type="text" name='lname'  placeholder='Enter LastName' />
+                </Form.Group>
+                <Form.Group className="mb-3 col-lg-6" controlId="formBasicEmail">
+                  <Form.Label>Email address</Form.Label>
+                  {/* value={inputdata.email} onChange={setInputValue} */}
+                  <Form.Control type="email" name='email'  placeholder='Enter Email' />
+                </Form.Group>
+                <Form.Group className="mb-3 col-lg-6" controlId="formBasicEmail">
+                  <Form.Label>Mobile</Form.Label>
+                  {/* value={inputdata.mobile} onChange={setInputValue} */}
+                  <Form.Control type="text" name='mobile'  placeholder='Enter Mobile' />
+                </Form.Group>
+                <Form.Group className="mb-3 col-lg-6" controlId="formBasicEmail">
+                  <Form.Label>Select Your Gender</Form.Label>
+                  {/* onChange={setInputValue} */}
+                  <Form.Check
+                    type={"radio"}
+                    label={`Male`}
+                    name="gender"
+                    value={"Male"}
+                    
+                  />
+                  {/* onChange={setInputValue} */}
+                  <Form.Check
+                    type={"radio"}
+                    label={`Female`}
+                    name="gender"
+                    value={"Female"}
+                    
+                  />
+                </Form.Group>
+                <Form.Group className="mb-3 col-lg-6" controlId="formBasicEmail">
+                  <Form.Label>Select Your Status</Form.Label>
+                  {/* onChange={setStatusValue} */}
+                  <Select options={options}   />                
+                  </Form.Group>
+                <Form.Group className="mb-3 col-lg-6" controlId="formBasicEmail">
+                  <Form.Label>Select Your Profile</Form.Label>
+                  {/* onChange={setProfile} */}
+                  <Form.Control type="file" name='user_profile'   placeholder='Select Your Profile' />
+                </Form.Group>
+                <Form.Group className="mb-3 col-lg-6" controlId="formBasicEmail">
+                  <Form.Label>Enter Your Location</Form.Label>
+                  {/* value={inputdata.location} onChange={setInputValue} */}
+                  <Form.Control type="text" name='location'  placeholder='Enter Your Location' />
+                </Form.Group>
+                {/* onClick={submitUserData} */}
+                <Button variant="primary" type="submit" >
+                  Submit
+                </Button>
+              </Row>
+
+            </Form>
+          </Card>
+          {/* <ToastContainer position="top-center" /> */}
+    </div>
+    </>
   )
 }
 
--- src/Pages/Profile/Profile.js
+++ src/Pages/Profile/Profile.js
@@ -1,9 +1,33 @@
 import React from 'react';
 import './Profile.css';
+import { Card,Row } from 'react-bootstrap';
 
 function Profile() {
   return (
-    <div>Profile</div>
+    <>
+    <div className="container">
+          <Card className='card-profile shadow col-lg-6 mx-auto mt-5'>
+            <Card.Body>
+              <Row>
+                <div className="col">
+                  <div className="card-profile-stats d-flex justify-content-center">
+                    <img src='https://cdn1.iconfinder.com/data/icons/avatars-1-5/136/88-512.png' alt="profile image" />
+                  </div>
+                </div>
+              </Row>
+              <div className='text-center'>
+                <h3>Max Miller</h3>
+                {/* {userprofile.fname + userprofile.lname} {userprofile.email} {userprofile.mobile} {userprofile.gender} {userprofile.location} {userprofile.status} {moment(userprofile.datecreated).format("DD-MM-YYYY")} {userprofile.dateUpdated} */}
+                <h4><i className="fa-solid fa-envelope email"></i>&nbsp;:- <span></span> </h4>
+                <h5><i className="fa-solid fa-mobile"></i>&nbsp;:- <span></span> </h5>
+                <h4><i className="fa-solid fa-person"></i>&nbsp;:- <span></span> </h4>
+                <h4><i className="fa-solid fa-location-pin location"></i>&nbsp;:- <span></span> </h4>
+                <h5>Status&nbsp;:- <span></span> </h5>
+              </div>
+            </Card.Body>
+          </Card>
+        </div>
+    </>
   )
 }
 
